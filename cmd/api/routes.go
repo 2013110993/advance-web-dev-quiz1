37569aecc83500e42de56d28f70d086c065d3487
federico rosado <2013110993@ub.edu.bz>
2022-09-11 10:37:13 -0600

    create routes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,1 +1,19 @@
-//File 
+// Filename cmd/api/routes.go
+
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) routes() *httprouter.Router {
+
+	router := httprouter.New()
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/entries", app.createEntryHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/entries/:id", app.showEntryHandler)
+
+	return router
+}
